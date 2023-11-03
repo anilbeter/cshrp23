99aa2c841ba7582f5fc2ecdf3f97c8ce08106e92
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-03 06:21:29 +0300

    List introduction

---
+++ Colllections/Program.cs
@@ -0,0 +1,24 @@
+﻿List<string> stringList = new();
+
+stringList.Add("First entry");
+stringList.Add("Second entry");
+stringList.Add("Third entry");
+
+Console.WriteLine(stringList[1]);
+// Second Entry
+
+// Shorter Way
+List<int> ints = new()
+{
+    23,
+    29,
+    35,
+    74,
+    84,
+    99
+};
+for (int i = 0; i < ints.Count; i++)
+{
+    Console.WriteLine(ints[i]);
+    // 23, 29, 35, 74, 84, 99
+}
