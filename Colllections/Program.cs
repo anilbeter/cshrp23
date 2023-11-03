8e4719ea42734caf9423a15b03b8f772d047bc27
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-03 19:53:14 +0300

    add my own solution for collections exercise

--- Colllections/Program.cs
+++ Colllections/Program.cs
@@ -1,24 +1,43 @@
-﻿List<string> stringList = new();
+﻿/*
+Exercise 1
+a) Declare an Integer-Array which can contain 20 values. Then use a For-Loop to set all those values to random numbers between 1 and 100.
+b) Do the same with an Integer-List.
+*/
 
-stringList.Add("First entry");
-stringList.Add("Second entry");
-stringList.Add("Third entry");
+using System.Reflection.Metadata;
 
-Console.WriteLine(stringList[1]);
-// Second Entry
-
-// Shorter Way
-List<int> ints = new()
+Random random = new();
+int[] randomNumbers = new int[20];
+for (int i = 0; i < randomNumbers.Length; i++)
 {
-    23,
-    29,
-    35,
-    74,
-    84,
-    99
-};
-for (int i = 0; i < ints.Count; i++)
+    int randomNum = random.Next(1, 101);
+    randomNumbers[i] = randomNum;
+    Console.WriteLine(randomNumbers[i]);
+}
+
+/*
+Exercise 2
+a) Declare a String-List and read Text from the console in an Endless-Loop.
+Whenever the player enters something add it to the List.
+
+b) When the player enters the string “stop” print every item in the list in the reversed order.
+i.e. the player enters:
+Bob
+Cat
+Monkey
+Stop
+
+You program should output:
+Monkey
+Cat
+Bob
+
+c) In addition the Program should sort the List and output the sorted list too.
+*/
+List<string> stringList = new();
+string stopKey = Console.ReadLine();
+while (stopKey != "stop")
 {
-    Console.WriteLine(ints[i]);
-    // 23, 29, 35, 74, 84, 99
+    stringList.Add(stopKey);
+    stopKey = Console.ReadLine();
 }
