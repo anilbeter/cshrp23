cf7cbd9ca5f4cd4266dfe51f864d98a92a19e7ce
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-04 21:13:07 +0300

    while loop

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rng = new Random();
+int score = 0;
+int dice1 = 0;
+while (dice1 != 1)
+{
+    dice1 = rng.Next(1, 7);
+    score += dice1;
+    Console.WriteLine("You rolled a " + dice1 + ". Current score: " + score);
+
+}
+Console.WriteLine("You rolled 1! Final score: " + score + "!");
